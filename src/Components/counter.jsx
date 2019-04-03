2699dc38367938d5d3e60ab085fd0000241f4292
jaasmaz <jaasmaz@yahoo.com>
2019-04-03 13:04:30 -0700

    Loops, Conditional Rendering, Events, State

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  // constructor() {
+  //   super();
+  //   this.handelIncrement = this.handelIncrement.bind(this);
+  // }
+
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
-  render() {
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no Tags!</p>;
+
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-      </div>
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul> //Works like *ngFor for Rendering Lists
     );
   }
+
   getBadgeClasses() {
     var classes = "badge m-2 badge-";
     classes += this.state.count > 0 ? "primary" : "warning";
@@ -21,6 +31,28 @@ class Counter extends Component {
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
+  }
+
+  handelIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handelIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <br />
+        {this.state.tags.length === 0 && "Please enter Tags !"}
+        {this.renderTags()}
+      </div>
+    );
   }
 }
 
