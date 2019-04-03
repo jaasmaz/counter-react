87eec6a53020d7f4d6c7947748b52ad45e769a64
jaasmaz <jaasmaz@yahoo.com>
2019-04-02 17:25:40 -0700

    added getBadgeClasses method

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200"
+    count: 0
   };
+
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    var classes = "badge m-2 badge-";
+    classes += this.state.count > 0 ? "primary" : "warning";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
